54c6cb9d882afe21a8a0ad7a87ec45cdfaf0ab8f
renzaspiras <aspirasrenz@gmail.com>
2025-07-10 00:13:48 +0000

    refined client 1.0

--- obj/Debug/net8.0/SIGLAT-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SIGLAT-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60958086fb9d4d3c3a50befd004944bea553a0cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3183ebb8fa7ca01c304065c3a133dcf15f4f0df")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
