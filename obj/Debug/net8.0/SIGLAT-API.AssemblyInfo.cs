d149804701e165901733f8d7876e56b4eb6001e9
renzaspiras <aspirasrenz@gmail.com>
2025-07-31 14:40:45 +0000

    adding status n the alertdto

--- obj/Debug/net8.0/SIGLAT-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SIGLAT-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d5d50393de1abef36a978602499b919ea08952")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18ab0a9f12970e09531a48585a879aadfc95db59")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
