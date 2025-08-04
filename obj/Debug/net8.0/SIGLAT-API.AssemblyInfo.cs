aa2c36c7cbf85661815a73e6a3f070eef8430040
renzaspiras <aspirasrenz@gmail.com>
2025-08-04 01:49:20 +0000

    chat system implemented, with table

--- obj/Debug/net8.0/SIGLAT-API.AssemblyInfo.cs
+++ obj/Debug/net8.0/SIGLAT-API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a81cb10599ad6a4136417f1e095ae5f4c0ea7bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be87f9f7b8c4965319cd6df2611e303e7a43f598")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIGLAT-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
